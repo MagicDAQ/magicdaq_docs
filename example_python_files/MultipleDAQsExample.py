574eaa9455d482ce4cd8163849244a65a3f0870a
sean <sean.hadley1@gmail.com>
2022-03-03 21:22:28 +1300

    Added MultipleDAQsExample.py

--- example_python_files/MultipleDAQsExample.py
+++ example_python_files/MultipleDAQsExample.py
@@ -1,4 +1,4 @@
-# Connect 1 or multiple MagicDAQs to your system using the USB cables.
+# Connect one or more MagicDAQs to your system using the USB cables.
 # Feel free to use a USB hub to connect multiple DAQs to your computer.
 
 # Import the standard time module
@@ -13,56 +13,58 @@ daq_one = MagicDAQDevice()
 # Get the serial numbers of all DAQs that are connected to your computer.
 daqs_serial_number_list = daq_one.list_all_daqs()
 print('List of DAQ Serial Numbers: '+str(daqs_serial_number_list))
-print('Thre are '+str(len(daqs_serial_number_list))+' DAQs in total connected to your system.')
+print('There are '+str(len(daqs_serial_number_list))+' DAQs in total connected to your computer.')
 print('')
+
 
 # If there is at least 1 DAQ, connect to it and read its serial number
 if len(daqs_serial_number_list) >= 1:
-    print('Attempting to connect to DAQ serial number: '+str(daqs_serial_number_list[0]))
+
+    # NOTE:
+    # For systems that always use the same DAQ units, you can hard code the serial numbers
+    # For example:
+    # daq_one.open_daq_device(daq_serial_number = '931cd19e')
+
     daq_one.open_daq_device(daq_serial_num = daqs_serial_number_list[0])
-    
+    # Read the DAQ's serial number
+    print('daq_one serial number: ', daq_one.get_serial_number())
+
+
     # You can now do useful things with daq_one
     # For example, you can set digital I/O pin P0.0 to HIGH
     print('Setting Pin P0.0 HIGH on daq_one')
     daq_one.set_digital_output(0,1)
     
-    # We will be sleeping to allow time to measure the P0.0 with a multimeter
-    # This flag indicates if the sleep period has been performed
-    sleep_has_hapened = False
-    
-    # Is there a 2nd DAQ connected to the System?
-    # If so, you can simultaneously connect to it and do stuff.
-    if len(daqs_serial_number_list) >= 2:
-        print('Attempting to connect to DAQ serial number: '+str(daqs_serial_number_list[1]))
-        
-        # You need to create another MagicDAQDevice object
-        daq_two = MagicDAQDevice()
-        # Connect to the 2nd DAQ
-        daq_two.open_daq_device(daq_serial_num = daqs_serial_number_list[1])
-        
-        # You can now do useful things with daq_two
-        # For example, you can set digital I/O pin P0.0 to LOW
-        print('Setting Pin P0.0 LOW on daq_two')
-        daq_two.set_digital_output(0,0)
-        
-        print ('Sleeping for 5 sec. to allow time to measure P0.0 pin with multimeter.')
-        time.sleep(5)
-        # Set the sleep_has_hapended flag to True so the sleep period is not repeated
-        sleep_has_hapened = True
-        
-        # We are done with daq_two so we can close it
-        print('Closing daq_two')
-        # Close daq_two
-        daq_two.close_daq_device()
-    
-    # If the sleep period has not already occurred, do it now
-    if not sleep_has_hapened:
-        print ('Sleeping for 5 sec. to allow time to measure P0.0 pin with multimeter.')
-        time.sleep(5)
-    
-    # We are done with daq_two so we can close it
-    print('Closing daq_one')
-    # Close daq_one
-    daq_one.close_daq_device()
 
-print ('Multiple DAQs Example Script COMPLETED.')
+# Is there a 2nd DAQ connected to the System?
+# If so, you can simultaneously connect to it and do stuff.
+if len(daqs_serial_number_list) >= 2:
+
+    # You need to create another MagicDAQDevice object
+    daq_two = MagicDAQDevice()
+    # Connect to the 2nd DAQ
+    daq_two.open_daq_device(daq_serial_num = daqs_serial_number_list[1])
+    # Read the DAQ's serial number
+    print('')
+    print('daq_two serial number: ', daq_two.get_serial_number())
+
+
+    # You can now do useful things with daq_two
+    # For example, you can set digital I/O pin P0.0 to LOW
+    print('Setting Pin P0.0 LOW on daq_two')
+    daq_two.set_digital_output(0,0)
+
+
+
+# Sleeping for 30 sec so you can measure the DAQ digital output pins with a multimeter
+print('')
+print('Sleeping for 30 sec so you can measure the digital output pins with a multimeter')
+time.sleep(30)
+
+# We need to close the DAQs
+if len(daqs_serial_number_list) >= 1:
+    daq_one.close_daq_device()
+if len(daqs_serial_number_list) >= 2:
+    daq_two.close_daq_device()
+
+
